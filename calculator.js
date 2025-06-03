59a3cff1d93e9587914bb24374a1da701c12f8ab
David Lazic <lazic.ddavid@gmail.com>
2025-06-03 18:02:03 +0200

    added const billInput

--- calculator.js
+++ calculator.js
@@ -1,3 +1,5 @@
 const btnElement = getElementById("calculate");
+const bullinput = document.getAnimations.getElementById("bill");
+const tipInput = document.getElementById("tip");
 
 function calculateTotal() {}

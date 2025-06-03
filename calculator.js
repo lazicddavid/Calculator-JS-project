a45a550698649db5e6c95b1bff400a4781844218
David Lazic <lazic.ddavid@gmail.com>
2025-06-03 18:03:50 +0200

    added click element

--- calculator.js
+++ calculator.js
@@ -1,5 +1,7 @@
 const btnElement = getElementById("calculate");
-const bullinput = document.getAnimations.getElementById("bill");
+const bullinput = document.getElementById("bill");
 const tipInput = document.getElementById("tip");
 
 function calculateTotal() {}
+
+btnElement.addEventListener("Click", calculateTotal);

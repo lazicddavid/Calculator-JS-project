9dabd20c2cedaa2cad138c06fd853c93615a10d4
David Lazic <lazic.ddavid@gmail.com>
2025-06-08 20:20:08 +0200

    added total, bil, tip

--- calculator.js
+++ calculator.js
@@ -33,10 +33,30 @@ btnElement.addEventListener("click", function () {
   totalSpan.textContent = total;
 });
 
-<getComputedStyle.da
-
-btnElement();
 //umesto da se desava na klik, neka se desava na eventSubmit, racucanje totala
 // da bi to uradio, moras imati formu, i na formu staviti EventListener submit
 
 //how to add submit Event on a form on JS
+
+let bill = 0;
+let tip = 0;
+let total = 0;
+
+const billInput = document.getElementById("bill");
+const tipInput = document.getElementById("tip");
+const totalSpan = document.getElementById("total");
+const form = document.getElementById("myForm");
+
+billInput.addEventListener("input", function () {
+  bill = Number(billInput.value);
+});
+
+tipInput.addEventListener("input", function () {
+  tip = Number(tipInput.value);
+});
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  total = bill * (1 + tip / 100);
+  totalSpan.textContent = total;
+});
